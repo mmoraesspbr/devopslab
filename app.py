bfbca76c9b50aeba9ddc7c6390b3d75fee53f2d3
mmoraesspbr <96797910+mmoraesspbr@users.noreply.github.com>
2023-04-04 19:33:28 -0300

    20-Bug Removed

--- app.py
+++ app.py
@@ -8,18 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Marcelo 24/03/2023 - Pipeline DevOps - v1"
-
-
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")  
-
+    return "Marcelo 04/04/2023 - Pipeline DevOps - v1"
 
 if __name__ == '__main__':
     app.run()
